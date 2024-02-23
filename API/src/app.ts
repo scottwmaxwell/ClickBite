16320ddaf8404dba5578d81643de3d39b26d871c
scottwmaxwell <scottwillardmaxwell@protonmail.com>
2024-02-23 16:40:52 -0700

    Created app

---
+++ API/src/app.ts
@@ -0,0 +1,17 @@
+import express, {Request, Response} from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const port = process.env.PORT;
+console.log(process.env)
+
+const app = express();
+
+app.get('/', (req: Request, res: Response)=>{
+    res.send('Hellooo!');
+});
+
+app.listen(port, ()=>{
+    console.log("Running in " + process.env.ENV + " mode")
+    console.log('Listening on http://localhost:' + port)
+});
